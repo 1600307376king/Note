ff5e567d7bace9f5d833ac5d4867d407d08f3a1a
1600307376king <1600307376@qq.com>
2019-11-05 17:34:38 +0800

    up

--- web/view/add_note.py
+++ web/view/add_note.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, url_for, jsonify, request, redirec
 add_index = Blueprint('add_page', __name__)
 
 
-@add_index.route('/add')
+@add_index.route('/add/')
 def home():
     return render_template('add_note.html')
 
